a065797dc0407dab437dd856439b65002ad787b0
kchen-tw <kchen.ntu@gmail.com>
2023-12-02 02:44:38 +0800

    Revert "First Edition"
    This reverts commit db221cc54760639f5fab122c2bf28277484a4676.

--- SharedMemorySize/kernel.cu
+++ SharedMemorySize/kernel.cu
@@ -1,37 +1,11 @@
 ﻿#include <iostream>
 #include <cuda_runtime.h>
 
-using namespace std;
-
-__global__ void kernel() {
-    // 這是一個簡單的 CUDA 核心，可以是你的實際計算
-}
-
 int main() {
     int sharedMemorySize;
-    
     cudaDeviceGetAttribute(&sharedMemorySize, cudaDevAttrMaxSharedMemoryPerBlock, 0);
 
-    cout << "Maximum shared memory size per block: " << sharedMemorySize << " bytes" << endl;
-
-    int device;
-    cudaGetDevice(&device);
-
-    int blockSize, minGridSize, gridSize;
-    cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, kernel, 0, 0);
-
-    int maxThreadsPerBlock;
-    cudaDeviceGetAttribute(&maxThreadsPerBlock, cudaDevAttrMaxThreadsPerBlock, device);
-
-    // 現在，你可以使用 minGridSize 和 blockSize 來計算 gridSize
-    gridSize = (1 + (minGridSize - 1) / blockSize);
-
-    cout << "Device: " << device << endl;
-    cout << "Max Threads Per Block :" << maxThreadsPerBlock << endl;
-    cout << "Maximum potential block size: " << blockSize << endl;
-    cout << "Minimum grid size: " << minGridSize << endl;
-    cout << "Computed grid size: " << gridSize << endl;
-
+    std::cout << "Maximum shared memory size per block: " << sharedMemorySize << " bytes" << std::endl;
 
     return 0;
 }
