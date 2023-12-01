aade5cd6c919155b7081e7cfd6d90991d9533d6b
kchen-tw <kchen.ntu@gmail.com>
2023-12-01 23:52:05 +0800

    First Edition

---
+++ SharedMemorySize/kernel.cu
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <cuda_runtime.h>
+
+int main() {
+    int sharedMemorySize;
+    cudaDeviceGetAttribute(&sharedMemorySize, cudaDevAttrMaxSharedMemoryPerBlock, 0);
+
+    std::cout << "Maximum shared memory size per block: " << sharedMemorySize << " bytes" << std::endl;
+
+    return 0;
+}
